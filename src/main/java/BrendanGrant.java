ead94cb7e323eddae1528c54b417d34898647106
SojourningSloop <grantbm@uci.edu>
2024-04-14 18:13:10 -0700

    Created BrendanGrant class and methods

---
+++ src/main/java/BrendanGrant.java
@@ -0,0 +1,36 @@
+
+public class BrendanGrant {
+/* Assignment 1:
+String getFullName(). This method returns your full name, with spacing, as a string.
+String getFirstName(). This method returns your first name as a string.
+String getLastName(). This method returns your last name as a string.
+String getUCInetID(). This method returns your UCInetID as a string.
+int getStudentNumber(). This method returns your UCI student number.
+String getRotatedFullName(int shift). This method returns your name, rotated (with wrap-around), by ’shift’ characters. Positive numbers rotate to the left, and negative numbers rotate to the right. For example, for my name “Jim Jones”, if I executed:
+ * */
+	
+	public String getFullName() {
+		return "Brendan Grant";
+	}
+	
+	public String getFirstName() {
+		return "Brendan";
+	}
+	
+	public String getLastName() {
+		return "Grant";
+	}
+	
+	public String getUCInetID() {
+		return "grantbm";
+	}
+	
+	public int getStudentNumber() {
+		return 82131203;
+	}
+	
+	public String getRotatedFullName(int shift) {
+		return null;
+	}
+	
+}
